8668bf1329b6530aac180f133ba5900ab1f60d76
koust007 <koustu007@gmail.com>
2023-10-11 12:30:20 -0700

    First Commit

---
+++ src/Basic/HelloWorld.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class HelloWorld {
+	public void greetings() {
+		System.out.println("This is my 1st java program");
+	}
+	
+	public static void main(String [] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.greetings();
+	}
+
+}
