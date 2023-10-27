dd194610264f59334a78ecb15907c3e294d786f5
koust007 <koustu007@gmail.com>
2023-10-27 11:02:20 -0700

    Added Sub method

--- src/Basic/Maths.java
+++ src/Basic/Maths.java
@@ -9,11 +9,27 @@ public class Maths {
 		
 	}
 	
+	public void multiple(int a , int b) {
+		int result = a * b;
+		System.out.println("Multiply of two numbers is :"+result);
+		
+		
+	}
+	
+	public void Sub(int a , int b) {
+		int result = a - b;
+		System.out.println("Sub of two numbers is :"+result);
+		
+		
+	}
+	
 	//Dev 2 has made this change
 	
 	public static void main(String[] args) {
 		Maths obj = new Maths();
 		obj.add(2, 5);
+		obj.multiple(2, 5);
+		obj.Sub(5, 2);
 
 	}
 
